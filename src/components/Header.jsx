fb896589ffb8b0d73143d4c50909aa3d18c0723c
guillemrima <117720260+guillemrima@users.noreply.github.com>
2023-05-28 09:32:01 +0200

    Fix: Fixed input from "NuevoPresupuesto"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import ControlPresupuesto from "./ControlPresupuesto"
 const Header = ({presupuesto, setPresupuesto, isValidPrespuesto, setIsValidPresupuesto}) => {
     return (
         <header>
-            <h1>Planificador de Gastos</h1>
+            <h1>Planificador de <span className="hero">Gastos</span></h1>
             {isValidPrespuesto ? (
                 <ControlPresupuesto 
                     presupuesto = {presupuesto}
